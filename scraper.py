8ea9ea7a4d83affcc58ace8dfec225e1d12fef86
RMEfan <anhngu@uw.edu>
2024-05-10 03:10:14 -0700

    Scraper - filter out abnormal matchgames
    Filter out warmups, showcases, disconnects
    
    Tourney regex now accepts period after "vs"

--- scraper.py
+++ scraper.py
@@ -18,7 +18,7 @@ db = sqlite3.connect("awesome.db")
 
 # a tournament match is defined as a multiplayer match whose name fits the format
 #   ABBR: (team 1) vs (team 2)
-tourney_re = re.compile(r".*: \(.*\) vs \(.*\)")
+tourney_re = re.compile(r".*: \(.*\) vs\.? \(.*\)")
 
 async def main():
     cur = db.cursor()
@@ -30,6 +30,7 @@ async def main():
     # 107474593 ocl gf
     start_id = 107474593
     ids = (107474593, 110682497)
+    # all ocl bracket w23
     idsids = (106688657,106688649,106684920,106647595,
               106669944,106682522,106684444,106643259,
               106686882,106687250,106691174,106670157,
@@ -44,11 +45,13 @@ async def main():
               106944371,106925641,107064293,107056176,
               107053416,107058578,107066561,107078386,
               107082999,107163395,107186478,107202847,107202335,107343096,107474593,)
+    # ocl qual w23
+    moreidsids = (106503364, 106508352, 106526178, 106526896, 106528049, 106530354, 106534327, 106531363, 106540332, 106541457, 106545033, 106549892, 106551091, 106553123, 106554423, 106555769)
     match = None
-    overwrite = True
-
+    overwrite = False
+    dest = "csvfiles/ocl_w23_withq.csv"
     if overwrite: 
-        with open("csvfiles/testing.csv", "w", newline='') as f:
+        with open(dest, "w", newline='') as f:
             writer = csv.writer(f)
             writer.writerow(("Match ID", 
                         "Datetime", 
@@ -57,17 +60,17 @@ async def main():
                         "Username", 
                         "Mods", 
                         "Score"))
-    for i in idsids:
+    for i in moreidsids:
         try:
-            time.sleep(.2)
+            time.sleep(.3)
             match_response = await api.match(i)
-        except ImportError:
+        except Exception:
             print()
             print(f"id {i} failed")
             continue
         #print()
         valid_match = re.fullmatch(tourney_re, match_response.match.name) != None
-        #print(f"is '{match_response.match.name}' a tourney match? {valid_match}")
+        print(f"is '{match_response.match.name}' a tourney match? {valid_match}")
         if not valid_match: continue
         #print(match_response)
         users = match_response.users
@@ -85,28 +88,46 @@ async def main():
         #print(f"first event id {fid}")
         #print(f"last event id  {lid}")
 
-        
+        # gather all of the maps played in this match
         lowest = 999999999999
         all_events = list()
         for event in events:
             lowest = min(lowest, event.id)
-            all_events.append(event)
+            if event.detail.type == MatchEventType.OTHER:
+                all_events.append(event)
         while lowest > fid:
             new_match_response = aapi.match(i, before=lowest)
+            for user in new_match_response.users:
+                user_dict[user.id] = user.username
             more_events = new_match_response.events
             for event in more_events:
                 if event.detail.type == MatchEventType.OTHER:
                     all_events.append(event)
                 lowest = min(lowest, event.id)
 
+        # skip if no maps were played
+        if not all_events: continue
 
+        # first pass - find the most common amount of players per map
+        playercounts = dict()
         for event in all_events:
-            lowest = min(lowest, event.id)
-            #earliest = min(earliest, event.id)
-            # print(f"id {event.id}")
-            if not event.detail.type == MatchEventType.OTHER:
-                # print("hey")
-                continue
+            if not event.detail.type == MatchEventType.OTHER: continue
+            num_players = len(event.game.scores)
+            if num_players not in playercounts:
+                playercounts[num_players] = 0
+            playercounts[num_players] += 1
+            #print(playercounts)
+        
+        most_common_player_count = max(playercounts, key=playercounts.get)
+        print(f"most common: {most_common_player_count}")
+            
+
+        # second pass - for each map, for each score, write a csv row about that score 
+        #     skip any maps with unusual player counts
+        for event in all_events:
+            # if not event.detail.type == MatchEventType.OTHER:
+            #     # print("hey")
+            #     continue
             game = event.game
             eid = event.id
             etime = event.timestamp
@@ -116,6 +137,7 @@ async def main():
             bm_id = game.beatmap_id
             mset = bm._beatmapset
             #print(f"\nevent id {eid} played {mset.artist} - {mset.title}")
+            if not len(game.scores) == most_common_player_count: continue
             for score in game.scores:
                 #print(f"{user_dict[score.user_id]} got {score.score} on {bm_id}")
                 line = (i, 
@@ -125,13 +147,13 @@ async def main():
                         user_dict[score.user_id], 
                         score.mods, 
                         score.score)
-                await write_csv(line)
+                await write_csv(line, dest)
     pass
 
 
-async def write_csv(obj):
+async def write_csv(obj, filename):
     # TODO: use append mode for multiple sessions
-    with open("csvfiles/testing.csv", "a", newline='') as f:
+    with open(filename, "a", newline='') as f:
 
         writer = csv.writer(f)
         writer.writerow(obj)
