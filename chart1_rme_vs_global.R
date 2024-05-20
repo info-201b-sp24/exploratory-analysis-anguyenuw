beff0c3addafb7f8a5a25ee7e6d5465b9e39270c
RMEfan <anhngu@uw.edu>
2024-05-19 18:09:02 -0700

    more typos, remove extra titles

--- chart1_rme_vs_global.R
+++ chart1_rme_vs_global.R
@@ -10,10 +10,10 @@ scatter <- ggplot(players, aes(x = GlobalRank, y = RME)) +
   geom_point(size = 0.7)
 scatter <- scatter + coord_cartesian(xlim = c(1,50000), ylim = c(500, 3300)) +
   scale_x_continuous(labels = scales::comma)
-scatter <- scatter + labs(title="Global Ranking VS RME Rating", 
+scatter <- scatter + labs(title="", 
                           subtitle="Players above rank 50,000 with at least 30 tournament scores from Jan 1 2023 to Aug 13 2023", 
                           y="RME Rating", 
-                          x="Global Rank (as of May 17 2024), log plot") 
+                          x="Global Rank (as of May 17 2024)") 
 
 
 scatter
@@ -22,7 +22,7 @@ scatter_log <- ggplot(players, aes(x = GlobalRank, y = RME)) +
   geom_point(size = 0.7)
 scatter_log <- scatter_log + coord_cartesian(xlim = c(1,1000000), ylim = c(500, 3300)) + 
   scale_x_continuous(trans='log10', labels = scales::comma)
-scatter_log <- scatter_log + labs(title="Global Ranking (log) VS RME Rating", 
+scatter_log <- scatter_log + labs(title="", 
                           subtitle="Players with at least 30 tournament scores from Jan 1 2023 to Aug 13 2023", 
                           y="RME Rating", 
                           x="Global Rank (as of May 17 2024), log plot") 
--- chart3_histogram_rme.R
+++ chart3_histogram_rme.R
@@ -6,7 +6,7 @@ players <- read.csv("csvfiles/user_data/users_with_RME.csv") %>%
   filter(Games > 30)
 
 RME_histogram <- ggplot(players, aes(x = RME)) + geom_histogram(binwidth = 50, colour = "black", fill = "lightblue") 
-RME_histogram <- RME_histogram + labs(title="Distribution of RME Ratings (up to Aug 13 2023)", 
+RME_histogram <- RME_histogram + labs(title="", 
                   subtitle="Players with at least 30 tournament scores from Jan 1 2023 to Aug 13 2023", 
                   y="Number of players", 
                   x="RME Rating")
